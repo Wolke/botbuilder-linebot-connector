401b71cf867729f89f11228c4ca89f6bd54a9b76
Charan Puvvala <charan.puvvala@gmail.com>
2017-06-26 18:10:46 +0530

    remove obsolete files

--- config/aakash.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	bot: {
-   appId:'95fffae9-a0a5-473b-add7-db4aac8b4722',
-    appPass: '371YtYtSx7hvwQ1H1Fa63qM'
-  }
-};
--- config/charan.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	bot: {
-    appId:'67fe37be-96dd-4264-9ffa-1c90d696c6b3',
-    appPass: '0AA7gPAydrxooigjm0vDWVZ'
-  }
-};
