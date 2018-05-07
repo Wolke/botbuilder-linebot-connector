0a1bfb5e33978b64e4d1715c309f284c1421c7e5
Wolke <wolkesau@gmail.com>
2018-05-07 19:58:23 +0800

    docs(2.2.14):

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.default = {
+    channelId: "1579199289",
+    channelSecret: "9c1d004121867cd188e4e62d0a3aabe6",
+    channelAccessToken: "XfNE40U2MPhO33xTY/SpC5IwASt2jxxRW6W6VAmN+89Ol3o2iVMm57fgkiYyY/ilxGo4yEctF1Bg3UFX9GirFN+OmfivqcbMQl0XL3U2tiiI2Lz5ONjM1jXkOi/m7/XcabMtKjoFsgKFkt1PqL6ZaQdB04t89/1O/w1cDnyilFU="
+}
